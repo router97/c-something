43a9c02c44c0c7605a5087b1e8bf25fbddb76dea
router97 <router97.io@gmail.com>
2026-06-16 20:25:56 +0400

    273

--- csapp/273.c
+++ csapp/273.c
@@ -1,13 +1,3 @@
-/*
-Write code for a function with the following prototype:
-Addition that saturates to TMin or TMax
-int saturating_add(int x, int y);
-Instead of overflowing the way normal two’s-complement addition does, saturating addition returns 
-TMax when there would be positive overflow, and TMin
-when there would be negative overflow. Saturating arithmetic is commonly used
-in programs that perform digital signal processing.
-Your function should follow the bit-level integer coding rules (page 164).
-*/
 #include <stdio.h>
 #include <assert.h>
 #include <limits.h>
