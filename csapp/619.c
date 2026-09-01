d0ad83437f3f333d9589609a36cc810b7efbe15d
router97 <router97.io@gmail.com>
2026-09-01 19:44:45 +0400

    619

---
+++ csapp/619.c
@@ -0,0 +1,95 @@
+Given the assumptions of Practice Problem 6.18, determine the cache performance of the following code:
+
+for (i = 31; i >= 0; i--){
+    for (j = 31; j >= 0; j--) {
+        total_x += grid[j][i].x;
+        total_y += grid[j][i].y;
+    }
+}
+
+A. What is the total number of reads?
+32 * 32 = 1024 elements in grid
+1 read per element
+1024 * 2 = 2048 reads
+
+
+B. What is the total number of reads that hit in the cache?
+
+with a 2,048-byte direct-mapped data cache with 32-byte blocks (B = 32).
+2048 / 32 = 64 lines
+
+Elem            Line        Elem            Line
+grid[0][0-3]    0           grid[1][0-3]    8
+grid[0][4-7]    1           grid[1][4-7]    9
+grid[0][8-11]   2           grid[1][8-11]   10
+grid[0][12-15]  3           grid[1][12-15]  11
+grid[0][16-19]  4           grid[1][16-19]  12
+grid[0][20-23]  5           grid[1][20-23]  13
+grid[0][24-27]  6           grid[1][24-27]  14
+grid[0][28-31]  7           grid[1][28-31]  15
+...
+grid[30][0-3]   48          grid[31][0-3]   56
+grid[30][4-7]   49          grid[31][4-7]   57
+grid[30][8-11]  50          grid[31][8-11]  58
+grid[30][12-15] 51          grid[31][12-15] 59
+grid[30][16-19] 52          grid[31][16-19] 60
+grid[30][20-23] 53          grid[31][20-23] 61
+grid[30][24-27] 54          grid[31][24-27] 62
+grid[30][28-31] 55          grid[31][28-31] 63
+
+ITER 0:
+OP          Elem            Status
+load        grid[31][0]     MISS :: load grid[31][0-3] into line 56
+load        grid[30][0]     MISS :: load grid[30][0-3] into line 48
+...
+load        grid[8][0]      MISS :: load grid[8][0-3] into line 0
+load        grid[7][0]      MISS :: load grid[7][0-3] into line 56
+load        grid[6][0]      MISS :: load grid[6][0-3] into line 48
+load        grid[5][0]      MISS :: load grid[5][0-3] into line 40
+load        grid[4][0]      MISS :: load grid[4][0-3] into line 32
+load        grid[3][0]      MISS :: load grid[3][0-3] into line 24
+load        grid[2][0]      MISS :: load grid[2][0-3] into line 16
+load        grid[1][0]      MISS :: load grid[1][0-3] into line 8
+load        grid[0][0]      MISS :: load grid[0][0-3] into line 0
+
+ITER 1:
+OP          Elem            Status
+load        grid[31][1]     MISS :: load grid[31][0-3] into line 56
+load        grid[30][1]     MISS :: load grid[30][0-3] into line 48
+...
+load        grid[8][0]      MISS :: load grid[8][0-3] into line 0
+load        grid[7][0]      MISS :: load grid[7][0-3] into line 56
+load        grid[6][0]      MISS :: load grid[6][0-3] into line 48
+load        grid[5][0]      MISS :: load grid[5][0-3] into line 40
+load        grid[4][0]      MISS :: load grid[4][0-3] into line 32
+load        grid[3][0]      MISS :: load grid[3][0-3] into line 24
+load        grid[2][0]      MISS :: load grid[2][0-3] into line 16
+load        grid[1][0]      MISS :: load grid[1][0-3] into line 8
+load        grid[0][0]      MISS :: load grid[0][0-3] into line 0
+
+...
+ITER 4:
+OP          Elem            Status
+load        grid[31][4]     MISS :: load grid[31][4-7] into line 57
+load        grid[30][4]     MISS :: load grid[30][4-7] into line 49
+...
+load        grid[8][4]      MISS :: load grid[8][4-7] into line 1
+load        grid[7][4]      MISS :: load grid[7][4-7] into line 57
+load        grid[6][4]      MISS :: load grid[6][4-7] into line 49
+load        grid[5][4]      MISS :: load grid[5][4-7] into line 41
+load        grid[4][4]      MISS :: load grid[4][4-7] into line 33
+load        grid[3][4]      MISS :: load grid[3][4-7] into line 25
+load        grid[2][4]      MISS :: load grid[2][4-7] into line 17
+load        grid[1][4]      MISS :: load grid[1][4-7] into line 9
+load        grid[0][4]      MISS :: load grid[0][4-7] into line 1
+
+
+C. What is the hit rate?
+total_x += grid[j][i].x; will always be a miss
+total_y += grid[j][i].y; will always be a hit
+
+
+D. What would the miss hit be if the cache were twice as big?
+cache lines will be overwritten every 16 instead of 8 rows,
+but that still isnt enough for the next iteration to use whats
+been loaded in the previous. Has to be 4 times as big
